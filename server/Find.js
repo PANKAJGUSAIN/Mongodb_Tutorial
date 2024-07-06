f63cef272e19af7faa2d79d07dfe8598a681baae
Pankaj Gusain <gusainpankaj1999@gmail.com>
2024-07-06 13:24:30 +0530

    learned comparison operator ,logical operator , quering

--- server/Find.js
+++ server/Find.js
@@ -29,6 +29,17 @@ async function FindSpecficDocuments(collection , socket) {
             projection: { _id: 0, user_socket_id : 1  },
         }
         const result = await collection.find(query , options ).toArray(); // convert all the doucment in a array
+        // const result = await collection.find({ user_rooms : {$eleMatch:{roomid:'123' }}})
+        // AND operator
+        // way1 - const result = await collection.find({$and : {"airlines" : "SouthWest"} , {"stop" : {$gt : "1"}  })
+        // way2 - const result = await collection.find( {"airlines" : "SouthWest"} , {"stop" : {$gt : "1"}} )
+        // or operator
+        // const result = await collection.find({$or : {"airlines" : "SouthWest"} , {"stop" : {$gt : "1"}  })
+        // combining and and or operator
+        // const result = await collection.find({ $and : [  { $or : [{src_airport : "SEA"} , {des_airport : "SEA"} ]  }    , { $or : [{airline:'1'} , {airline:'2'}]  }   ] })
+        // okay so why in above query we didn't do on "or" "or" like find({ $or :[]  , $or:[]}) ? 
+        // this is because in object we cannot have two key with same name so send or will override the first one 
+        // and hence, we must using explicit $or or $and operator
 
         socket.emit('all_session' ,result)
         return result
